fe8150abab297c3d33f8d6ac4a6afe5ab4dbfd3a
Aaron Cerezo <106458947+PastaSus@users.noreply.github.com>
2025-05-03 18:15:13 +0800

    'update markup and wrapped js in domcontentloaded'

--- script.js
+++ script.js
@@ -1,58 +1,61 @@
 "use strict";
 
-const newsletterMain = document.querySelector(".newsletter__main");
-const newsletterVerification = document.querySelector(
-  ".newsletter__verification"
-);
-
-const subscribeBtn = document.querySelector(".newsletter__subscribe");
-const dismissBtn = document.querySelector(".newsletter__dismiss");
-
-const form = document.querySelector(".newsletter__form");
-const newsletterEmail = document.querySelector(
-  ".newsletter__confirmation-email"
-);
-
-const emailInput = document.querySelector(".newsletter__input");
-
-const errorMsg = document.querySelector(".error-message");
-
 // goals: use custom, use constraint validation api, use 3rd party
 
 // this is using custom
-function isValidEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
+// Approach 1: wrap your entire file in an event listener
+document.addEventListener("DOMContentLoaded", function () {
+  const newsletterMain = document.querySelector(".newsletter__main");
+  const newsletterVerification = document.querySelector(
+    ".newsletter__verification"
+  );
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  // const subscribeBtn = document.querySelector(".newsletter__subscribe");
+  const dismissBtn = document.querySelector(".newsletter__dismiss");
 
-  const email = emailInput.value.trim();
+  const form = document.querySelector(".newsletter__form");
+  const newsletterEmail = document.querySelector(
+    ".newsletter__confirmation-email"
+  );
 
-  if (!email) {
-    emailInput.classList.add("invalid");
+  const emailInput = document.querySelector(".newsletter__input");
 
-    errorMsg.textContent = "Email is required";
-  } else if (!isValidEmail(email)) {
-    emailInput.classList.add("invalid");
+  const errorMsg = document.querySelector(".error-message");
 
-    errorMsg.textContent = "Valid email required";
-  } else {
-    emailInput.classList.remove("invalid");
-    emailInput.classList.add("valid");
-
-    errorMsg.textContent = "";
-    newsletterEmail.textContent = emailInput.value;
-
-    newsletterMain.style.display = "none";
-    newsletterVerification.style.display = "grid";
+  function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
   }
-});
 
-dismissBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  newsletterMain.style.display = "grid";
+    const email = emailInput.value.trim();
 
-  newsletterVerification.style.display = "none";
+    if (!email) {
+      emailInput.classList.add("invalid");
+
+      errorMsg.textContent = "Email is required";
+    } else if (!isValidEmail(email)) {
+      emailInput.classList.add("invalid");
+
+      errorMsg.textContent = "Valid email required";
+    } else {
+      emailInput.classList.remove("invalid");
+      emailInput.classList.add("valid");
+
+      errorMsg.textContent = "";
+      newsletterEmail.textContent = emailInput.value;
+
+      newsletterMain.style.display = "none";
+      newsletterVerification.style.display = "grid";
+    }
+  });
+
+  dismissBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    newsletterMain.style.display = "grid";
+
+    newsletterVerification.style.display = "none";
+  });
 });
